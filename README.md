90afff29ceea39796544372206a6ed6c74489f07
Rafael Figueiredo Zanetti <rafaelfzanetti@hotmail.com>
2024-02-28 21:38:42 -0300

    First Commit
    Creating project

---
+++ README.md
@@ -0,0 +1,2 @@
+# RocketSeat-Challenge01-CourseAPI
+ An API for courses based on the coding challenge made by RocketSeat on their Java & Spring Bootcamp
---
+++ src/test/java/br/com/rocketseat/springboot/challenges/courses/coursesapi/CoursesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.springboot.challenges.courses.coursesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
